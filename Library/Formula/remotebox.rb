43dc57802c6dd25171217257b9a46fd949facb35
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-26 15:24:14 +0800

    BROKEN: FileUtils does not work, bin/ not created

---
+++ Library/Formula/remotebox.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Remotebox < Formula
+  homepage 'https://nobgoblin.org.uk/'
+  url 'http://knobgoblin.org.uk/downloads/RemoteBox-1.6.tar.bz2'
+  sha1 '3e73e722f1bfddae04b9465d28b1958d2caf0ee0'
+
+
+  depends_on 'SOAP::Lite' 		=> :perl
+  depends_on 'rdesktop'			=> :recommended
+
+
+  def install
+    prefix.install Dir['*']
+    bin.mkdir
+	bin.install 'remotebox'
+  end
+end
