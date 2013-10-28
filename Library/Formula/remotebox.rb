ad1d647ff2f53cbb014aa49e456b94071c3f0926
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-29 01:20:00 +0800

    Patch Perl script to resolve symlink and refer to proper directory structure

--- Library/Formula/remotebox.rb
+++ Library/Formula/remotebox.rb
@@ -10,9 +10,42 @@ class Remotebox < Formula
   depends_on 'Gtk2'				=> :perl
   depends_on 'rdesktop'			=> :recommended
 
-
+  def patch
+  	# Perl script uses $Bin which does not resolve to symlink. 
+  	# Change to #RealBin - resolve symlink - and proper paths
+  	DATA
+  end
+  
   def install
     bin.install Dir['remotebox']
 	prefix.install Dir['*']
   end
 end
+
+__END__
+--- /usr/local/bin/remotebox	2013-10-19 15:45:38.000000000 +0800
++++ /tmp/remotebox.MODIFIED	2013-10-29 01:06:31.000000000 +0800
+@@ -3,19 +3,19 @@
+ # RemoteBox v1.6 (c) 2010-2013 Ian Chapman. Licenced under the terms of the GPL
+ use strict;
+ use warnings;
+-use FindBin qw($Bin);
++use FindBin qw($RealBin);
+ use POSIX qw(ceil);
+ use File::Basename;
+ use File::Spec;
+ use MIME::Base64;
+ # *** PACKAGERS MAY WISH TO PATCH THIS LOCATION ***
+-use lib "$Bin/share/remotebox";
++use lib "$RealBin/../share/remotebox";
+ # *************************************************
+ use vboxService qw($endpoint $fault :all);
+ 
+ # *** PACKAGERS MAY WISH TO PATCH THESE LOCATIONS ***
+-our $sharedir = "$Bin/share/remotebox";
+-our $docdir   = "$Bin/docs";
++our $sharedir = "$RealBin/../share/remotebox";
++our $docdir   = "$RealBin/../docs";
+ # ***************************************************
+ 
+ require 'vboxserializers.pl';
